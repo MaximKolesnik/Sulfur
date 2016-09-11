84f022d52d0af6f57f5f9ee2509e390fbca84e40
MaximKolesnik <maxim.kolesnik90@gmail.com>
2016-09-10 22:14:12 -0700

    Fixed merging errors

--- src/SulfurEngine/Settings/EngineSettings.h
+++ src/SulfurEngine/Settings/EngineSettings.h
@@ -6,12 +6,18 @@ namespace Sulfur
 {
   namespace EngineSettings
   {
-    // Logging
-    const std::string LogDir = "logs\\";
-    const bool ShowConsole   = true;
+    //Paths
+    const std::string LogDir = "Logs\\";
 
-    // SlotMap
+    //SlotMap
     const INT64 SlotMapObjsPerPage    = 512;
     const INT64 SlotMapInitNumOfPages = 3;
+
+    //Frame
+    const Real DefaultFPS = Real(1.0) / 60;
+
+    //Fiber
+    const UINT64 FiberStackSize = 0;          //If 0, uses default stack size for exe
+    const UINT64 FiberReservedStackSize = 0;  //If 0, uses default reserved stack size for exe
   };
 }
